92464acc22deecdb2c71ee8a9c0614d4b97c677e
Alberto <alberto.dosreis@georgebrown.ca>
2023-06-21 14:18:25 -0400

    removed hard coded section and left randomizer

--- app.js
+++ app.js
@@ -5,21 +5,14 @@ const app = Vue.createApp({
             lastName: 'Reis',
             email: 'albertoreis@gmail.com',
             gender: 'male',
-            pictureHC: 'https://randomuser.me/api/portraits/men/10.jpg',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
         }
     },
     methods: {
-        async getUserHardCoded() {
-            this.firstName = 'Michelle'
-            this.lastName = 'Colangelo'
-            this.email = 'michellecolangelo@hotmail.com'
-            this.gender = 'female'
-            this.pictureHC = 'https://randomuser.me/api/portraits/women/10.jpg'
-        },
         async getUserRandom(){
             const res = await fetch('https://randomuser.me/api')
             const {results} = await res.json()
-            
+
             this.firstName = results[0].name.first
             this.lastName = results[0].name.last
             this.email = results[0].email
@@ -30,6 +23,4 @@ const app = Vue.createApp({
 
 })
 
-app.mount('#app') // we are passing app as the selector
-
-// data is a function, that returns an object
+app.mount('#app')
