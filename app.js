3adc74a69caa7b35f3990a64be815fdb4675c02b
Alberto <alberto.dosreis@georgebrown.ca>
2023-06-21 13:43:35 -0400

    adding the first user

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const app = Vue.createApp({
-    template: '<h1>Hello World</h1>'
+    data() {
+        return {
+            firstName: 'Alberto',
+            lastName: 'Reis',
+            email: 'albertoreis@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+        }
+    }
 })
 
 app.mount('#app') // we are passing app as the selector
+
+// data is a function, that returns an object
